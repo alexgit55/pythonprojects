e9e317fe2d418ea5777c33fd6434e35c89827bc3
alexgit55 <121723219+alexgit55@users.noreply.github.com>
2025-06-27 11:59:18 -0400

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """validation.py contains validation functions to be used to verify input data from the user"""
 
+
 def ValidateUserInput(text, validChoices):
   """
   Prompts the user for input until a valid choice from the provided list is entered.
@@ -13,19 +14,20 @@ def ValidateUserInput(text, validChoices):
     The user's choice (string, lowercased).
   """
 
-  if not hasattr(validChoices,'__iter__'):
+  if not hasattr(validChoices, '__iter__'):
     raise TypeError("ValidChoices must be iterable")
 
-  userChoice='userChoice'
+  userChoice = 'userChoice'
   while userChoice.lower() not in validChoices:
-    userChoice=input(text)
+    userChoice = input(text)
     if userChoice.lower() in validChoices:
       break
     print("That is not a valid choice")
 
   return userChoice.lower()
 
-def GetIntValue(text):
+
+def GetIntValue(text, min=0, max=100):
   """
   Prompts the user for an integer value until a valid integer value is entered.
 
@@ -37,16 +39,21 @@ def GetIntValue(text):
   """
   while True:
     try:
-        intValue=int(input(f"{text}"))
-        break
+      intValue = int(input(f"{text}"))
+      assert intValue >= min and intValue <= max
+      break
     except ValueError:
-        print("That is not a valid number")
+      print("That is not a valid number")
+    except AssertionError:
+      print(f"Value must be between {min} and {max} ")
 
   return intValue
 
+
 if __name__ == '__main__':
-  userInput=ValidateUserInput("Please select from the following choices (y/n): ", ('y','n'))
+  userInput = ValidateUserInput(
+      "Please select from the following choices (y/n): ", ('y', 'n'))
   print(userInput)
 
-  userInt=GetIntValue("Enter a value to verify functionality: ")
+  userInt = GetIntValue("Enter a value to verify functionality: ")
   print(userInt)
