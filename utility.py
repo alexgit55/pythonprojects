69260a35165dc66ff572c1cf68c8cbe38d30d07f
alexgit55 <121723219+alexgit55@users.noreply.github.com>
2025-06-27 11:50:36 -0400

    added basic folder selection function

--- utility.py
+++ utility.py
@@ -2,6 +2,8 @@
 """utility.py contains custom functions to be used in other scripts/modules"""
 
 import os
+import tkinter as tk
+from tkinter import filedialog
 
 def clear_console():
   """
@@ -11,3 +13,12 @@ def clear_console():
       os.system('cls')  # For Windows
   else:
       os.system('clear')  # For Unix/Linux/Mac
+
+def select_folder(title="Select Folder"):
+    """
+    Opens a folder selection dialog and returns the selected folder path.
+    """
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    folder_path = filedialog.askdirectory(title=title)
+    return folder_path
